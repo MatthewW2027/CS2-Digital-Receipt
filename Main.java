69fc7fdc9a4b24d316adad7ebf82f33f7b2c485a
Matthew <149590049+MatthewW2027@users.noreply.github.com>
2025-10-24 12:55:33 +0000

    started project

--- Main.java
+++ Main.java
@@ -1,6 +1,29 @@
 public class Main {
 
    public static void main(String []args) {
-      System.out.println("Hello World");
+       
+      String highSchoolName = "The Birch Wathen Lenox School"; 
+      double drink = 1.50; 
+      double candy = 1.25; 
+      double hotDog = 2.75; 
+      double hamburger = 3.50; 
+      int orderNumber = (int)(Math.random()*3); 
+      int numOfDrinksOrdered = (int)(Math.random()*3); 
+      int numOfCandiesOrdered = (int)(Math.random()*3); 
+      int numOfHotDogs = (int)(Math.random()*3); 
+      int numOfHamburgers = (int)(Math.random()*3); 
+      int taxRate = 
+      
+      // RECEIPT PRINTOUT SECTION
+     System.out.println("**************************************");
+     System.out.println("*                                    *");
+     System.out.println("*     High School Snack Bar          *");
+     System.out.println("*                                    *");
+     System.out.println("*     Drink .........." + drink + "            *");                      
+     System.out.println("*     Candy .........." + candy + "           *");     
+     System.out.println("*     Hot Dog ........" + hotDog + "           *");     
+     System.out.println("*     Hamburger ......" + hamburger + "            *");     
+     System.out.println("*                                    *");    
+     System.out.println("**************************************");
    }
 }
