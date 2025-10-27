c1f0dfeed4c7526d485d93a63adf8f030f0090dc
Matthew <149590049+MatthewW2027@users.noreply.github.com>
2025-10-27 13:40:21 +0000

    worked on part C 3

--- Main.java
+++ Main.java
@@ -7,20 +7,23 @@ public class Main {
       double candy = 1.25; 
       double hotDog = 2.75; 
       double hamburger = 3.50; 
-      int orderNumber = (int)(Math.random()*3); 
+      int orderNumber = (int)(Math.random()*100)+1; 
       int numOfDrinksOrdered = (int)(Math.random()*3); 
       int numOfCandiesOrdered = (int)(Math.random()*3); 
       int numOfHotDogs = (int)(Math.random()*3); 
       int numOfHamburgers = (int)(Math.random()*3); 
-      int taxRate = 
+      double taxRate = 0.08; 
+      double subTotal = drink + candy + hotDog + hamburger; 
+      double totalTax = subTotal * taxRate; 
+      double total = subTotal + totalTax; 
       
       // RECEIPT PRINTOUT SECTION
      System.out.println("**************************************");
-     System.out.println("*                                    *");
+     System.out.println("*                  " + orderNumber + "                *");
      System.out.println("*     High School Snack Bar          *");
      System.out.println("*                                    *");
-     System.out.println("*     Drink .........." + drink + "            *");                      
-     System.out.println("*     Candy .........." + candy + "           *");     
+     System.out.println("* Drink .........." + drink + " quantity: " + numOfDrinksOrdered + "    *");                      
+     System.out.println("* Candy .........." + candy + " quantity " + numOfCandiesOrdered + "    *");     
      System.out.println("*     Hot Dog ........" + hotDog + "           *");     
      System.out.println("*     Hamburger ......" + hamburger + "            *");     
      System.out.println("*                                    *");    
